2561aa40950a0967062ae6a1a88f53f622e3ace9
takeru-frasco <hrkwtkr@gmail.com>
2021-08-16 21:47:07 +0900

    Create HelloC

--- App.jsx
+++ App.jsx
@@ -2,15 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Hello from './src/components/Hello';
+
 export default function App() {
+  // HTMLにあたる部分
   return (
     <View style={styles.container}>
+      <Hello bang={false}>World</Hello>
+      <Hello bang style={{ fontSize: 16 }}>Small world</Hello>
       <Text>Open up App.js to start working on your app!</Text>
+      {/* eslint-disable-next-line */}
       <StatusBar style="auto" />
     </View>
   );
 }
 
+// CSSnにあたる部分
 const styles = StyleSheet.create({
   container: {
     flex: 1,
